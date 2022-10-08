f7c4a940c7bdbe271b611ff8665804d808aa6a02
mxyen <cardenas.mayen.diana@gmail.com>
2022-10-07 22:43:56 -0500

    formatee el JS

--- js/des.js
+++ js/des.js
@@ -1,81 +1,73 @@
-function cifraDes(){
-    let txt = document.getElementById("txtDes").value;
-    console.log("El mensaje dice: "+txt);
+function cifraDes() {
+  let txt = document.getElementById("txtDes").value;
+  console.log("El mensaje dice: " + txt);
 
-    let claveDes = document.getElementById("clavedes").value;
-    if(claveDes.length != 8){
-        alert("La clave tiene que ser de 8 caracteres");
-        return false;
-    }
+  let claveDes = document.getElementById("clavedes").value;
+  if (claveDes.length != 8) {
+    alert("La clave tiene que ser de 8 caracteres");
+    return false;
+  }
 
-    let cifra =  CryptoJS.DES.encrypt(txt,claveDes);
+  let cifra = CryptoJS.DES.encrypt(txt, claveDes);
 
-    descargarArchivo(generarTexto(cifra), 'CifradoDES.txt');
+  descargarArchivo(generarTexto(cifra), "CifradoDES.txt");
 }
 
-function descifraDes(){
-    let cifrado = document.getElementById("txtDes").value;
-    console.log("Cifrado en DES: "+cifrado);
+function descifraDes() {
+  let cifrado = document.getElementById("txtDes").value;
+  console.log("Cifrado en DES: " + cifrado);
 
-    let claveDes = document.getElementById("clavedes").value;
-    if(claveDes.length != 8){
-        alert("La clave debe contener exactamente 8 caracteres .____.");
-        return false;
-    }
+  let claveDes = document.getElementById("clavedes").value;
+  if (claveDes.length != 8) {
+    alert("La clave debe contener exactamente 8 caracteres .____.");
+    return false;
+  }
 
-    let desci = CryptoJS.DES.decrypt(cifrado,claveDes);
-    desci = desci.toString(CryptoJS.enc.Utf8);
-    descargarArchivo(generarTexto(desci), 'DescifradoDES.txt');
+  let desci = CryptoJS.DES.decrypt(cifrado, claveDes);
+  desci = desci.toString(CryptoJS.enc.Utf8);
+  descargarArchivo(generarTexto(desci), "DescifradoDES.txt");
 }
 
+function leerdes() {
+  let archivodes = document.getElementById("archivodes").files[0];
 
-function leerdes(){
-    let archivodes = document.getElementById("archivodes").files[0];
+  let readerDes = new FileReader();
+  readerDes.onload = function (fileLoadedEvent) {
+    let txtDes = fileLoadedEvent.target.result;
+    document.getElementById("txtDes").value = txtDes;
+  };
 
-    let readerDes = new FileReader();
-    readerDes.onload = function(fileLoadedEvent){
-        let txtDes = fileLoadedEvent.target.result;
-        document.getElementById("txtDes").value = txtDes;
-    };
-
-    readerDes.readAsText(archivodes, "UTF-8");
-
+  readerDes.readAsText(archivodes, "UTF-8");
 }
-
-
 
 function descargarArchivo(contenidoEnBlob, nombreArchivo) {
+  var reader = new FileReader();
 
-    var reader = new FileReader();
+  reader.onload = function (event) {
+    var save = document.createElement("a");
+    save.href = event.target.result;
+    save.target = "_blank";
 
-    reader.onload = function (event) {
+    save.download = nombreArchivo || "CifradoDes.txt";
+    var clicEvent = new MouseEvent("click", {
+      view: window,
+      bubbles: true,
+      cancelable: true,
+    });
 
-        var save = document.createElement('a');
-        save.href = event.target.result;
-        save.target = '_blank';
+    save.dispatchEvent(clicEvent);
 
-        save.download = nombreArchivo || 'CifradoDes.txt';
-        var clicEvent = new MouseEvent('click', {
-            'view': window,
-            'bubbles': true,
-            'cancelable': true
-        });
+    (window.URL || window.webkitURL).revokeObjectURL(save.href);
+  };
 
-        save.dispatchEvent(clicEvent);
-
-        (window.URL || window.webkitURL).revokeObjectURL(save.href);
-    };
-
-    reader.readAsDataURL(contenidoEnBlob);
-};
-
+  reader.readAsDataURL(contenidoEnBlob);
+}
 
 function generarTexto(datos) {
-    let texto = [];
-    texto.push(datos);
+  let texto = [];
+  texto.push(datos);
 
-
-    return new Blob(texto, {
-        type: 'text/plain'
-    });
-};
+  return new Blob(texto, {
+    type: "text/plain",
+  });
+}
